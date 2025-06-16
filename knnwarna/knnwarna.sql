df86bfd32ae2bde3033ad413a2c47b848db078eb
Harry Witriyono <49424961+HarryWitriyono@users.noreply.github.com>
2025-06-16 13:44:17 +0700

    Update knnwarna.sql

--- knnwarna/knnwarna.sql
+++ knnwarna/knnwarna.sql
@@ -3,19 +3,13 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 03, 2025 at 02:55 AM
+-- Generation Time: Jun 16, 2025 at 08:43 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `knnwarna`
@@ -28,17 +22,30 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `datatraining` (
-  `IdData` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `IdData` int(11) NOT NULL,
+  `Kecerahan` double(5,2) DEFAULT NULL,
+  `Kejenuhan` double(5,2) DEFAULT NULL,
+  `Kelas` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `datatraining0`
+--
+
+CREATE TABLE `datatraining0` (
+  `IdData` int(11) NOT NULL,
   `Kecerahan` double(5,2) DEFAULT NULL,
   `Kejenuhan` double(5,2) DEFAULT NULL,
   `Kelas` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `datatraining`
+-- Dumping data for table `datatraining0`
 --
 
-INSERT INTO `datatraining` (`IdData`, `Kecerahan`, `Kejenuhan`, `Kelas`) VALUES
+INSERT INTO `datatraining0` (`IdData`, `Kecerahan`, `Kejenuhan`, `Kelas`) VALUES
 (1, 40.00, 20.00, 'Merah'),
 (2, 50.00, 50.00, 'Biru'),
 (3, 60.00, 90.00, 'Biru'),
@@ -54,11 +61,70 @@ INSERT INTO `datatraining` (`IdData`, `Kecerahan`, `Kejenuhan`, `Kelas`) VALUES
 --
 
 CREATE TABLE `datauji` (
-  `IdData` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `IdData` int(11) NOT NULL,
   `Kecerahan` double(5,2) DEFAULT NULL,
   `Kejenuhan` double(5,2) DEFAULT NULL,
-  `Kelas` varchar(15) NOT NULL,
+  `Kelas` varchar(15) DEFAULT NULL,
   `K` int(2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `datauji`
+--
+
+INSERT INTO `datauji` (`IdData`, `Kecerahan`, `Kejenuhan`, `Kelas`, `K`) VALUES
+(2, 50.00, 50.00, 'Biru', 5),
+(3, 20.00, 35.00, 'Merah', 5),
+(4, 40.00, 70.00, 'Biru', 5);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hasil`
+--
+
+CREATE TABLE `hasil` (
+  `IdData` int(2) NOT NULL,
+  `Jarak` double(5,2) DEFAULT NULL,
+  `Kelas` varchar(15) DEFAULT NULL,
+  `Jumlah` int(5) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kelas`
+--
+
+CREATE TABLE `kelas` (
+  `IdKelas` int(11) NOT NULL,
+  `Kelas` varchar(15) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `kelas`
+--
+
+INSERT INTO `kelas` (`IdKelas`, `Kelas`) VALUES
+(1, 'Merah'),
+(2, 'Biru'),
+(3, 'Kuning'),
+(4, 'Ungu'),
+(5, 'Hitam'),
+(6, 'hijau');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `perhitungan`
+--
+
+CREATE TABLE `perhitungan` (
+  `IdData` int(2) NOT NULL,
+  `Kecerahan` double(5,2) DEFAULT NULL,
+  `Kejenuhan` double(5,2) DEFAULT NULL,
+  `Jarak` double(5,2) DEFAULT NULL,
+  `Kelas` varchar(15) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -68,14 +134,32 @@ CREATE TABLE `datauji` (
 --
 -- Indexes for table `datatraining`
 --
--- ALTER TABLE `datatraining`
---  ADD PRIMARY KEY (`IdData`);
+ALTER TABLE `datatraining`
+  ADD PRIMARY KEY (`IdData`);
+
+--
+-- Indexes for table `datatraining0`
+--
+ALTER TABLE `datatraining0`
+  ADD PRIMARY KEY (`IdData`);
 
 --
 -- Indexes for table `datauji`
 --
---ALTER TABLE `datauji`
- -- ADD PRIMARY KEY (`IdData`);
+ALTER TABLE `datauji`
+  ADD PRIMARY KEY (`IdData`);
+
+--
+-- Indexes for table `hasil`
+--
+ALTER TABLE `hasil`
+  ADD KEY `IdData` (`IdData`);
+
+--
+-- Indexes for table `kelas`
+--
+ALTER TABLE `kelas`
+  ADD PRIMARY KEY (`IdKelas`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -84,10 +168,24 @@ CREATE TABLE `datauji` (
 --
 -- AUTO_INCREMENT for table `datatraining`
 --
---ALTER TABLE `datatraining`
---  MODIFY `IdData` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
---COMMIT;
+ALTER TABLE `datatraining`
+  MODIFY `IdData` int(11) NOT NULL AUTO_INCREMENT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+--
+-- AUTO_INCREMENT for table `datatraining0`
+--
+ALTER TABLE `datatraining0`
+  MODIFY `IdData` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `datauji`
+--
+ALTER TABLE `datauji`
+  MODIFY `IdData` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `kelas`
+--
+ALTER TABLE `kelas`
+  MODIFY `IdKelas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
